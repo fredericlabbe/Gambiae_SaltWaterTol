d447bb25d61f10823c84b1ebabc8a67009732db1
Frederic Labbe <frederic.labbe.phd@gmail.com>
2020-04-04 15:08:57 -0400

    Add files via upload

---
+++ FltProGapMis.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 09:24:01 2019
+@author: Frederic Labbe
+Filtering sequence alignments (in phylip format) having fewer than a maximum proportion of gap and missing nucleotides (e.g. 50%).
+usage: python FltProGapMis.py --prop 0.5
+"""
+
+import os
+import os.path
+import glob
+import shutil
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', "--prop", type = float, required = True, help = 'maximum proportion of gap in the alignment')
+args = parser.parse_args()
+
+def FltProGapMis(prop):
+    current_directory = os.getcwd()
+    keep_directory = os.path.join(current_directory, r'1_Keep')
+    if not os.path.exists(keep_directory):
+        os.makedirs(keep_directory)  
+        rem_directory = os.path.join(current_directory, r'2_Remove')
+    if not os.path.exists(rem_directory):
+        os.makedirs(rem_directory)
+    files = glob.glob('*.phy')
+    for file in files:
+        with open(file, 'r') as phy:
+            header = phy.readline()
+            headsp = header.split()
+            length = int(headsp[1])
+            maxgap = int(length * prop)
+            nbgaps = list()
+            for line in phy:
+                x = line.split()
+                x[1] = x[1].replace("N", "-")
+                gap = x[1].count('-')
+                nbgaps.append(int(gap))
+            nbgaps.sort(reverse = True)
+        if nbgaps[0] < maxgap:
+            shutil.move(file, keep_directory)
+        else:
+            shutil.move(file, rem_directory)
+
+if __name__ == '__main__':
+    FltProGapMis(args.prop)
