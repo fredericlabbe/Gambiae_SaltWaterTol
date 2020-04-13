aba477d39b09b43141517f7ea521b93d75849c22
Frederic Labbe <frederic.labbe.phd@gmail.com>
2020-04-12 23:50:17 -0400

    Add files via upload

---
+++ FastaCrop.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 16:50:47 2020
+Cropping alignment in FASTA format according to a BED file.
+usage: python FastaCrop.py
+@author: frederic
+"""
+
+import re
+import glob
+import shutil
+import os.path
+from Bio import SeqIO
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', "--ref", type = str, required = True, help = 'reference species')
+args = parser.parse_args()
+
+def FastaCrop(ref):
+    current_directory = os.getcwd()
+    crop_directory = os.path.join(current_directory, r'CDS')
+    if not os.path.exists(crop_directory):
+        os.makedirs(crop_directory)
+    files = glob.glob('*.fa')
+    for file1 in files:
+        chrom1 = file1.split("_")[0]
+        start1 = int(file1.split("_")[1])
+        stop1 = file1.split("_")[2]
+        stop1 = int(stop1.replace(".fa", ""))
+        file2 = file1.replace(".fa", "_CDS.bed")
+        bed = open(file2, 'r')
+        for line in bed:
+            line.split()
+            line = re.split(r'\t+', line)
+            chrom2 = line[0]
+            start2 = int(line[1])
+            stop2 = line[2]
+            stop2 = int(stop2.replace("\n", ""))
+        if chrom1 == chrom2:
+            if start1 != start2 or stop1 != stop2:
+                startdif = abs(start1 - start2)
+                stopdif = abs(stop1 - stop2)
+                Outputfile = "{}_{}_{}_CDS.fa".format(chrom1, start1 + startdif, stop1 - stopdif)
+                f = open(Outputfile, 'w')
+                fasta_sequences = SeqIO.parse(file1, 'fasta')
+                for fasta in fasta_sequences:
+                    header, sequence = fasta.id, str(fasta.seq)
+                    if header == ref:
+                        stopgap = sequence[len(sequence) - stopdif:len(sequence)].count('-')
+                        sequence = sequence
+                        beginning = 0
+                        startgap = 0
+                        while startgap < startdif and beginning < len(sequence):
+                            if sequence[beginning] == '-':
+                                beginning = beginning + 1                                
+                            else:
+                                startgap = startgap + 1
+                                beginning = beginning + 1
+                    sequence2 = sequence[beginning:(len(sequence) - stopdif - stopgap)]
+                    f.write(">{}\n{}\n".format(header, sequence2))
+                f.close()
+                shutil.move(Outputfile, crop_directory)
+            else:
+                Outputfile = "{}_{}_{}_CDS.fa".format(chrom1, start1, stop1)
+                f = open(Outputfile, 'w')
+                fasta_sequences = SeqIO.parse(file1, 'fasta')
+                for fasta in fasta_sequences:
+                    header, sequence = fasta.id, str(fasta.seq)
+                    f.write(">{}\n{}\n".format(header, sequence))
+                f.close()
+                shutil.move(Outputfile, crop_directory)
+
+if __name__ == '__main__':
+    FastaCrop(args.ref)
