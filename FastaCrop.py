c93e741ffe1a01c317a8d4f8f6af57109d7cda03
Frederic Labbe <frederic.labbe.phd@gmail.com>
2020-04-20 17:03:40 -0400

    Add files via upload

--- FastaCrop.py
+++ FastaCrop.py
@@ -37,45 +37,45 @@ def FastaCrop(ref):
             start2 = int(line[1])
             stop2 = line[2]
             stop2 = int(stop2.replace("\n", ""))
-        if chrom1 == chrom2:
-            if start1 != start2 or stop1 != stop2:
-                startdif = abs(start1 - start2)
-                stopdif = abs(stop1 - stop2)
-                Outputfile = "{}_{}_{}_CDS.fa".format(chrom1, start1 + startdif, stop1 - stopdif)
-                f = open(Outputfile, 'w')
-                fasta_sequences = SeqIO.parse(file1, 'fasta')
-                for fasta in fasta_sequences:
-                    header, sequence = fasta.id, str(fasta.seq)
-                    if header == ref:
-                        stopping = len(sequence) - 1
-                        stopgap = 0
-                        while stopgap < stopdif and stopping > 0:
-                            if sequence[stopping] == '-':
-                                stopping = stopping - 1                                
-                            else:
-                                stopgap = stopgap + 1
-                                stopping = stopping - 1
-                        beginning = 0
-                        startgap = 0
-                        while startgap < startdif and beginning < len(sequence):
-                            if sequence[beginning] == '-':
-                                beginning = beginning + 1                                
-                            else:
-                                startgap = startgap + 1
-                                beginning = beginning + 1
-                    sequence2 = sequence[beginning:stopping + 1]
-                    f.write(">{}\n{}\n".format(header, sequence2))
-                f.close()
-                shutil.move(Outputfile, crop_directory)
-            else:
-                Outputfile = "{}_{}_{}_CDS.fa".format(chrom1, start1, stop1)
-                f = open(Outputfile, 'w')
-                fasta_sequences = SeqIO.parse(file1, 'fasta')
-                for fasta in fasta_sequences:
-                    header, sequence = fasta.id, str(fasta.seq)
-                    f.write(">{}\n{}\n".format(header, sequence))
-                f.close()
-                shutil.move(Outputfile, crop_directory)
+            if chrom1 == chrom2:
+                if start1 != start2 or stop1 != stop2:
+                    startdif = abs(start1 - start2)
+                    stopdif = abs(stop1 - stop2)
+                    Outputfile = "{}_{}_{}_CDS.fa".format(chrom1, start1 + startdif, stop1 - stopdif)
+                    f = open(Outputfile, 'w')
+                    fasta_sequences = SeqIO.parse(file1, 'fasta')
+                    for fasta in fasta_sequences:
+                        header, sequence = fasta.id, str(fasta.seq)
+                        if header == ref:
+                            stopping = len(sequence) - 1
+                            stopgap = 0
+                            while stopgap < stopdif and stopping > 0:
+                                if sequence[stopping] == '-':
+                                    stopping = stopping - 1                                
+                                else:
+                                    stopgap = stopgap + 1
+                                    stopping = stopping - 1
+                            beginning = 0
+                            startgap = 0
+                            while startgap < startdif and beginning < len(sequence):
+                                if sequence[beginning] == '-':
+                                    beginning = beginning + 1                                
+                                else:
+                                    startgap = startgap + 1
+                                    beginning = beginning + 1
+                        sequence2 = sequence[beginning:stopping + 1]
+                        f.write(">{}\n{}\n".format(header, sequence2))
+                    f.close()
+                    shutil.move(Outputfile, crop_directory)
+                else:
+                    Outputfile = "{}_{}_{}_CDS.fa".format(chrom1, start1, stop1)
+                    f = open(Outputfile, 'w')
+                    fasta_sequences = SeqIO.parse(file1, 'fasta')
+                    for fasta in fasta_sequences:
+                        header, sequence = fasta.id, str(fasta.seq)
+                        f.write(">{}\n{}\n".format(header, sequence))
+                    f.close()
+                    shutil.move(Outputfile, crop_directory)
 
 if __name__ == '__main__':
     FastaCrop(args.ref)
